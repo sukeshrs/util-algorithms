397967dad691aed650a88c9960e62a20453f858f
sukeshrs <sukesrs@gmail.com>
2024-05-20 05:41:20 -0700

    Find duplicates using constant space

--- src/main/java/com/util/algo/array/FindDuplicates.java
+++ src/main/java/com/util/algo/array/FindDuplicates.java
@@ -6,13 +6,31 @@ public class FindDuplicates {
     public List<Integer> findDuplicates(int[] input) {
 
         List<Integer> list = new LinkedList<>();
-        Set<Integer> allNumbersSet =  new HashSet<>();
+        Set<Integer> allNumbersSet = new HashSet<>();
 
-        for(int i=0 ;i< input.length ; i++){
-            if(allNumbersSet.contains(input[i])){
+        for (int i = 0; i < input.length; i++) {
+            if (allNumbersSet.contains(input[i])) {
                 list.add(input[i]);
             }
             allNumbersSet.add(input[i]);
+        }
+        return list;
+    }
+
+    public List<Integer> findDuplicatesConstantSpace(int[] input) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int num : input) {
+            //Find the index for the number. The index is 0 based and numbers are from 1-n so need to subtract 1
+            //The array could contain negative values, so Math.abs is needed to convert to positive number.
+            System.out.println(num);
+            int index = Math.abs(num) - 1;
+
+            if (input[index] < 0) {
+                list.add(num);
+            } else {
+                input[index] = -input[index];
+            }
         }
         return list;
     }
--- src/test/java/com/util/algo/array/FindDuplicatesTest.java
+++ src/test/java/com/util/algo/array/FindDuplicatesTest.java
@@ -1,8 +1,12 @@
 package com.util.algo.array;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class FindDuplicatesTest {
 
@@ -19,6 +23,20 @@ public class FindDuplicatesTest {
         int[] input = {8,8,8,8,8,8,};
         List<Integer> duplicates = findDuplicates.findDuplicates(input);
         System.out.println(duplicates);
+    }
+
+    @Test
+    public void itTestFindDuplicateWithConstantSpace(){
+        int[] input = {3,5,6,3,7,8,1,2};
+        List<Integer> dups = findDuplicates.findDuplicatesConstantSpace(input);
+        Assertions.assertTrue(dups.contains(3));
+    }
+
+    @Test
+    public void itTestFindDuplicateWithConstantSpace_One_element(){
+        int[] input = {1};
+        List<Integer> dups = findDuplicates.findDuplicatesConstantSpace(input);
+        Assertions.assertFalse(dups.contains(1));
     }
 
 
